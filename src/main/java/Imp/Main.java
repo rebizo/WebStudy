5cd4771676a3c6e82ea146641c8edbcc60a7e8c0
gs.dzhura <dzhura.gs@minsvyazdnr.ru>
2018-08-06 10:09:18 +0300

    Initial commit

---
+++ src/main/java/Imp/Main.java
@@ -0,0 +1,23 @@
+package Imp;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import services.AllRequestsServlet;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*"/*"/authform"*/);
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
+}
