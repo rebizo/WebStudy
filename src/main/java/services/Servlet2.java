1b5dbe6875840ea1a500f11697cd09ee9353e9c3
gs.dzhura <dzhura.gs@minsvyazdnr.ru>
2018-08-29 11:08:01 +0300

    29.08.18 Finale
    Signed-off-by: gs.dzhura <dzhura.gs@minsvyazdnr.ru>

--- src/main/java/services/Servlet2.java
+++ src/main/java/services/Servlet2.java
@@ -1,6 +1,5 @@
 /*
 package services;
-
 import templater.PageGenerator;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,31 +8,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 public class Servlet2 extends HttpServlet {
-
     public class AllRequestsServlet extends HttpServlet {
-
         public void doGet(HttpServletRequest request,
                           HttpServletResponse response) throws ServletException, IOException {
-
             Map<String, Object> pageVariables = createPageVariablesMap(request);
             pageVariables.put("message", "");
-
             //response.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
             response.getWriter().println(pageVariables.get("key").toString());
-
              if (pageVariables.containsKey("value")) {
                 response.getWriter().println(pageVariables.get("value"));
             } else {
                 response.getWriter().println(PageGenerator.instance().getPage("page.html", pageVariables));
             }
-
             response.setContentType("text/html;charset=utf-8");
             response.setStatus(HttpServletResponse.SC_OK);
-
         }
-
         public Map<String, Object> createPageVariablesMap(HttpServletRequest request) { // static was
             Map<String, Object> pageVariables = new HashMap<>();
             pageVariables.put("method", request.getMethod());
@@ -43,6 +33,5 @@ public class Servlet2 extends HttpServlet {
             pageVariables.put("parameters", request.getParameterMap().toString());
             return pageVariables;//dsa
         }
-
     }
 }*/
